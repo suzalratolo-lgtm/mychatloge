7f2e9f5caa0372bc1292808e7b7d0df078edf24a
suzalratolo-lgtm <226949096+suzalratolo-lgtm@users.noreply.github.com>
2026-02-15 10:44:08 +0530

    main.py

--- main.py
+++ main.py
@@ -58,6 +58,9 @@ async def main():
     # Run forever
     await client.run_until_disconnected()
 
+# PASTE THIS INSTEAD:
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
